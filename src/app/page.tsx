588faba9a5bfe5e0ef91a8f7caa968cb9b2a6b30
nutawud <nutawud103@outlook.co.th>
2025-12-23 11:04:22 +0700

    update5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-// import { redirect } from "next/navigation";
-
-// export default function Home() {
-//   redirect("/dashboard");
-// }
-
 export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>HOME OK</h1>
-    </div>
-  );
+  return <h1>HOME OK ON VERCEL</h1>;
 }
