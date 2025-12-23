279ef667cf2486fc650a7050622bfa19980d573b
nutawud <nutawud103@outlook.co.th>
2025-12-23 10:37:25 +0700

    update4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
+
+// export default function Home() {
+//   redirect("/dashboard");
+// }
 
 export default function Home() {
-  redirect("/dashboard");
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>HOME OK</h1>
+    </div>
+  );
 }
